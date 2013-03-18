fb123ff5e0694be03d48c7f8ce3d2aa815bdea93
Florian Reimair <florian.reimair@iaik.tugraz.at>
2013-03-18 10:20:17 +0100

    editing strokes works with persistence

--- code/Model.cs
+++ code/Model.cs
@@ -77,27 +77,58 @@ namespace code
 			return new List<Array> ();
 		}
 
-		public void edit (List<Array> before, List<Array> after)
+		/*
+		 * http://www.w3.org/TR/SVG/struct.html
+		 * 
+		 * <svg width="12cm" height="4cm" viewBox="0 0 1200 400" xmlns="http://www.w3.org/2000/svg" version="1.1">
+		 *     <desc><tags><tag>jour fixe</tag><tag>skytrust</tag></tags></desc>
+		 *     <polyline fill="none" stroke="blue" stroke-width="10" points="50,375
+         *          150,375 150,325 250,325 250,375
+         *          350,375 350,250 450,250 450,375
+         *          550,375 550,175 650,175 650,375
+         *          750,375 750,100 850,100 850,375
+         *          950,375 950,25 1050,25 1050,375
+         *          1150,375" />
+         * </svg>
+         */
+		public void edit (List<int[]> before, List<int[]> after)
 		{
-			// http://www.w3.org/TR/SVG/struct.html
-//			<svg width="12cm" height="4cm" viewBox="0 0 1200 400"
-//     xmlns="http://www.w3.org/2000/svg" version="1.1">
-//  <desc><tags><tag>jour fixe</tag><tag>skytrust</tag></tags></desc>
-//
-//  <!-- Show outline of canvas using 'rect' element -->
-//  <rect x="1" y="1" width="1198" height="398"
-//        fill="none" stroke="blue" stroke-width="2" />
-//
-//  <polyline fill="none" stroke="blue" stroke-width="10" 
-//            points="50,375
-//                    150,375 150,325 250,325 250,375
-//                    350,375 350,250 450,250 450,375
-//                    550,375 550,175 650,175 650,375
-//                    750,375 750,100 850,100 850,375
-//                    950,375 950,25 1050,25 1050,375
-//                    1150,375" />
-//</svg>
+			// remove deprecated polylines
+			foreach (int[] current in before) {
+				// locate node
+				String pattern = "";
+				for (int i = 0; i < current.Length; i += 2)
+					pattern += current [i] + "," + current [i + 1] + " ";
+				XmlNode currentNode = root.SelectSingleNode ("/svg/polyline[@points='" + pattern.Trim () + "']");
 
+				// remove node
+				root.RemoveChild (currentNode);
+			}
+
+			// add new polylines
+			foreach (int[] current in after) {
+				XmlAttribute fillAttribute = document.CreateAttribute ("fill");
+				fillAttribute.Value = "none";
+
+				XmlAttribute strokeAttribute = document.CreateAttribute ("stroke");
+				strokeAttribute.Value = "black";
+
+				XmlAttribute strokeWidthAttribute = document.CreateAttribute ("stroke-width");
+				strokeWidthAttribute.Value = "10";
+
+				XmlAttribute pointsAttribute = document.CreateAttribute ("points");
+				for (int i = 0; i < current.Length; i += 2)
+					pointsAttribute.Value += current [i] + "," + current [i + 1] + " ";
+				pointsAttribute.Value = pointsAttribute.Value.Trim ();
+
+				XmlNode currentNode = document.CreateElement ("polyline");
+
+				currentNode.Attributes.Append (fillAttribute);
+				currentNode.Attributes.Append (strokeAttribute);
+				currentNode.Attributes.Append (strokeWidthAttribute);
+				currentNode.Attributes.Append (pointsAttribute);
+				root.AppendChild (currentNode);
+			}
 		}
 
 		public DateTime getCreationTimestamp ()
@@ -113,6 +144,9 @@ namespace code
 
 		public void persist ()
 		{
+			// check and adjust boundaries
+			// TODO check and adjust boundaries
+
 			// check if data directory exists and create if neccessary
 			if (!Directory.Exists (Environment.GetFolderPath (Environment.SpecialFolder.Personal) + "/.aJournal/"))
 				Directory.CreateDirectory (Environment.GetFolderPath (Environment.SpecialFolder.Personal) + "/.aJournal/");
@@ -123,10 +157,22 @@ namespace code
 		public static int Main (string[] args)
 		{
 			Entry DUT = new Entry ();
+			List<int[]> listA = new List<int[]> ();
+			listA.Add (new int[] {1,1,1,1,1,1});
+			listA.Add (new int[] {2,2,2,2,2,2});
+
+			List<int[]> listB = new List<int[]> ();
+			listB.Add (new int[] {3,3,3,3,3,3});
+			listB.Add (new int[] {4,4,4,4,4,4});
+
+			DUT.edit (new List<int[]> (), listA);
+			DUT.edit (listA, listB);
+			DUT.edit (listB, new List<int[]> ());
+			DUT.edit (new List<int[]> (), listA);
+
 			DUT.persist ();
 			return 0;
 		}
-
 	}
 }
 
